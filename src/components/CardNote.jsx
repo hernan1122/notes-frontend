d4de1fd2e4781e450faa990106618d75283784f1
Axel Torrico <torricohernan2@gmail.com>
2023-09-04 21:08:30 -0300

    se creo logica para ver a CardOptions

--- src/components/CardNote.jsx
+++ src/components/CardNote.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/CardNote.css'
 
+//icons
+import { BsThreeDots } from 'react-icons/bs'
+import CardOptions from './CardOptions';
+
 function CardNote() {
+  const [showOptions, setShowOptions] = useState(false)
+
   return (
     <div className='CardNote'>
-      <h4>Titulo</h4>
+      <div
+        className='CardNote-container-title'
+        onClick={() => setShowOptions(!showOptions)}
+      >
+        <h4>Titulo</h4>
+        <BsThreeDots className='CardNote-icon' />
+      </div>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos harum dolorum deleniti pariatur consequuntur at, cumque.</p>
+      <CardOptions show={showOptions} />
     </div>
   );
 }
