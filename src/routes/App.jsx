2a4a1d49e8163351e650c76cb800877176d16d3d
Axel Torrico <torricohernan2@gmail.com>
2023-09-04 21:06:11 -0300

    se llamo a AppProvider y se creo ruta /edit-note

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AppProvider from '../context/Context';
 import Start from '../containers/Start';
 import Home from '../containers/Home';
 import Signup from '../containers/Signup';
 import Login from '../containers/Login';
+import EditNote from '../components/EditNote';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Start />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <AppProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Start />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/edit-note' element={<EditNote />} />
+        </Routes>
+      </BrowserRouter>
+    </AppProvider>
   );
 }
 
